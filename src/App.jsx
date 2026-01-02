9a7b25b6d90af9b7ac3631bb960f95100c9791cd
Aurindom Ghosh <aurindomghosh24654@gmail.com>
2026-01-02 09:49:46 +0530

    logo placement

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import FormWizard from './components/FormWizard';
 import './index.css';
+import logo from './image (1).png';
 
 function App() {
     return (
         <div className="App">
+            <img src={logo} alt="MTC Logo" className="top-logo" />
             <FormWizard />
+            <footer className="site-footer">Microsoft Tech Community</footer>
         </div>
     )
 }
-
 export default App;
